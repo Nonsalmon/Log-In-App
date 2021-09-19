0a772e061405fb0f18a55f43357bf6015b5b97bd
Nonsalmon <fxred727@gmail.com>
2021-09-20 00:25:53 +0700

    Log in App

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView username = findViewById(R.id.username_editText);
+        TextView email = findViewById(R.id.email_editTextText);
+        TextView password =(TextView) findViewById(R.id.password_editText);
+
+        Button loginbtn = (Button) findViewById(R.id.login_btn);
+
+        //admin and admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("Sam") && password.getText().toString().equals("test12345") &&email.getText().toString().equals("test01@gmail.com")){
+                    //correct
+                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect
+                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
